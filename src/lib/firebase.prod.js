56bd5174aa4f08fc64cabf6658840d0c2d1e37ac
Joblyn <oaikhenahjob@gmail.com>
2020-10-29 22:16:35 +0100

    connected with firebase

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,7 +1,7 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import { seedDatabase } from '../seed';
+// import { seedDatabase } from '../seed';
 
 const config = {
   apiKey: "AIzaSyDQdS_KKBPk6tFTSYzuwUvIG5jk5vSa-KE",
@@ -15,7 +15,7 @@ const config = {
 
 const firebase = Firebase.initializeApp(config);
 
-seedDatabase(firebase);
+// seedDatabase(firebase);
 
 export { firebase }; 
 
