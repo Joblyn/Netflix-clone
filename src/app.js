4210a0a4f0a547ebfe23edb3f33ec4464ffb39e9
Joblyn <oaikhenahjob@gmail.com>
2020-11-09 19:21:04 +0100

    update

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import { useAuthListener} from './hooks';
 
 export default function App() {
   const { user } = useAuthListener();
-  // const user = false;
+
   return (
     <Router>
       <Switch>
